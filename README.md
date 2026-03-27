73943cd3fc2346ab2ea899de70b5dad5bcdb4199
JGreenwell <jsgreenwell83@gmail.com>
2026-03-27 13:40:22 -0500

    switching to card games

---
+++ README.md
@@ -0,0 +1,2 @@
+## Simple Game Project
+This is a series of simple games - made by students to practice Java and GitHub as part of Millikin University coursework.
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,73 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scan = new Scanner(System.in);
+        short choice = 100;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (choice != 9) {
+            // Card games are listed from easiest to program to hardest
+            System.out.print("""
+                    Hello and Welcome to the main menu!
+                    Please select from the following options (or 9 to exit):
+                        0: BlackJack
+                        1: Go Fish
+                        2: Solitaire
+                        3: Poker
+                        4: Uno
+                        5: Rummy
+                        9: Exit
+                    Enter the number of your selection:
+                    """);
+
+            choice = Short.parseShort(scan.nextLine());
+            switch (choice) {
+                case 0:
+                    BlackJack blackJack = new BlackJack();
+                    while (blackJack.keepPlaying) {
+                        blackJack.run();
+                        blackJack.checkExit();
+                    }
+                    break;
+                case 1:
+                    Fish gofish = new Fish();
+                    while (gofish.keepPlaying) {
+                        gofish.run();
+                        gofish.checkExit();
+                    }
+                    break;
+                case 2:
+                    Solitaire sol = new Solitaire();
+                    while (sol.keepPlaying) {
+                        sol.run();
+                        sol.checkExit();
+                    }
+                    break;
+                case 3:
+                    Poker poker = new Poker();
+                    while (poker.keepPlaying) {
+                        poker.run();
+                        poker.checkExit();
+                    }
+                    break;
+                case 4:
+                    Uno uno = new Uno();
+                    while (uno.keepPlaying) {
+                        uno.run();
+                        uno.checkExit();
+                    }
+                    break;
+                case 5:
+                    Rummy rummy = new Rummy();
+                    while (rummy.keepPlaying) {
+                        rummy.run();
+                        rummy.checkExit();
+                    }
+                    break;
+                default:
+                    System.out.println("Thanks for playing!\nGoodbye!");
+                    choice = 9; // set to exit
+            }
         }
     }
 }
